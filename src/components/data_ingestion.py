035643b94ab172c3aa287759a3a75dc30620bf50
Amallmmd <iammuhammedamal@gmail.com>
2024-02-08 00:29:35 +0530

    Working on EDA and Model Training Completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import src.exception import CustomException
+import src.logger import logging
+
+from dataclass import dataclass
+from sklearn.model_selection import train_test_split
